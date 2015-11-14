2a2f04803ecf957f9eaaec29ca2661b011410849
JoshCantor <josh.s.cantor@gmail.com>
2015-11-13 17:23:54 -0800

    retrieves albums and tracks

--- javascript/app.js
+++ javascript/app.js
@@ -7,6 +7,18 @@
   })
 })();
 
+// var uniq = function (list) {
+//   var result = []
+//   list.forEach(function(item) {
+//     if(result.indexOf(item) === -1) {
+//       result.push(item);
+//     }
+//   })
+//   return result;
+// }
+
+
+
 /**
   This function bootstraps the spotify request functionality.
 */
@@ -31,7 +43,6 @@ function bootstrapSpotifySearch(){
       // (We could have used the success callback directly)
       spotifyQueryRequest.done(function (data) {
         var artists = data.artists;
-
         // Clear the output area
         outputArea.html('');
 
@@ -59,11 +70,61 @@ function bootstrapSpotifySearch(){
 /* COMPLETE THIS FUNCTION! */
 function displayAlbumsAndTracks(event) {
   var appendToMe = $('#albums-and-tracks');
+  var artistId = $(event.target).attr('data-spotify-id');
+  var artistAlbumRequest = $.ajax({
+      url: 'https://api.spotify.com/v1/artists/' + artistId + '/albums',
+      method: "GET"
+  })
+  
+  artistAlbumRequest.done(function(data) {
+    var albums = data.items;
 
-  // These two lines can be deleted. They're mostly for show. 
-  console.log("you clicked on:");
-  console.log($(event.target).attr('data-spotify-id'));//.attr('data-spotify-id'));
+    appendToMe.html('');
+    
+    albums.forEach(function(album) {
+      var artistAlbumRequest = $.ajax({
+        url: 'https://api.spotify.com/v1/albums/' + album.id,
+        method: "GET"
+      })
+
+      artistAlbumRequest.done(function(data) {
+        var albumLi = $('<li id=' + album.id + '>' + data.name + ' (Release Date: ' + data.release_date + ')</li>');
+        albumLi.attr('data-album-id', album.id);
+        appendToMe.append(albumLi);
+        albumLi.click(displayTracks);
+      })
+    })
+  })
+
+    artistAlbumRequest.fail(function (error) {
+        console.log("Something Failed During Album Request:")
+        console.log(error);
+  })
+  
 }
+
+function displayTracks(event) {
+  var albumId = $(this).attr('id');
+  var appendToMe = $('#' + albumId);
+  var songClass = albumId
+  var orderedList = $('<ol class=' + songClass + '></ol>')
+  appendToMe.append(orderedList);
+  var albumId = $(event.target).attr('data-album-id');
+  var albumTrackRequest = $.ajax({
+      url: 'https://api.spotify.com/v1/albums/' + albumId,
+      method: "GET"
+    })
+  
+  albumTrackRequest.done(function(data) {
+    var songs = data.tracks.items;
+    songs.forEach(function(song) {
+      var songLi = $('<li>' + song.name + '</li>');
+      var jquerySongClass = '.' + songClass;
+      $(jquerySongClass).append(songLi);
+    })
+  })
+}
+
 
 /* YOU MAY WANT TO CREATE HELPER FUNCTIONS OF YOUR OWN */
 /* THEN CALL THEM OR REFERENCE THEM FROM displayAlbumsAndTracks */
